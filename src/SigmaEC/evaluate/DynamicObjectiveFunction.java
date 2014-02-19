18737f3885dadfab5cf516ad2a35b448331a586d
SigmaX <fyedernoggersnodden@gmail.com>
2014-02-19 12:07:49 -0500

    Add interface for use with dynamic objectives.

---
+++ src/SigmaEC/evaluate/DynamicObjectiveFunction.java
@@ -0,0 +1,10 @@
+package SigmaEC.evaluate;
+
+/**
+ *
+ * @author Eric 'Siggy' Scott
+ * @author Jeff Bassett
+ */
+public interface DynamicObjectiveFunction extends ObjectiveFunction {
+    void setGeneration(int i);
+}
