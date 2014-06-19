d3a8150caa354c082a6abf60888b8afaf6a14a67
SigmaX <fyedernoggersnodden@gmail.com>
2014-06-19 12:42:38 -0400

    Remove unused import.

--- src/SigmaEC/util/Parameters.java
+++ src/SigmaEC/util/Parameters.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
