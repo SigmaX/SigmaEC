42a30d4fd028a193b03e149f7b380ac064159883
SigmaX <fyedernoggersnodden@gmail.com>
2014-10-09 18:23:23 -0400

    Add standard methods to DoubleGene, fixing a bug revealed by tests.

--- src/SigmaEC/represent/DoubleGene.java
+++ src/SigmaEC/represent/DoubleGene.java
@@ -1,5 +1,7 @@
 package SigmaEC.represent;
 
+import SigmaEC.util.Misc;
+
 /**
  * A Gene that wraps a single double.
  * 
@@ -8,5 +10,32 @@ package SigmaEC.represent;
 public class DoubleGene implements Gene
 {
     final public double value;
-    public DoubleGene(double value) { this.value = value; }
+    public DoubleGene(double value) { this.value = value; assert(repOK()); }
+    
+    // <editor-fold defaultstate="collapsed" desc="Standard Methods">
+    
+    public final boolean repOK() {
+        return true;
+    }
+    
+    @Override
+    public boolean equals(final Object o) {
+        if (!(o instanceof DoubleGene))
+            return false;
+        final DoubleGene ref = (DoubleGene) o;
+        return Misc.doubleEquals(value, ref.value);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + (int) (Double.doubleToLongBits(this.value) ^ (Double.doubleToLongBits(this.value) >>> 32));
+        return hash;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("[%s: value=%f]", this.getClass().getSimpleName(), value);
+    }
+    // </editor-fold>
 }
