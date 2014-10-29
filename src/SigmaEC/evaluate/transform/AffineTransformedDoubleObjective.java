6c2543f11c927dd37fa451a9fc2513f44d1c6980
SigmaX <fyedernoggersnodden@gmail.com>
2014-10-29 15:24:33 -0400

    Modernize AffineTransformedDoubleObjective to work with the property file mechanism.

--- src/SigmaEC/evaluate/transform/AffineTransformedDoubleObjective.java
+++ src/SigmaEC/evaluate/transform/AffineTransformedDoubleObjective.java
@@ -3,6 +3,7 @@ package SigmaEC.evaluate.transform;
 import SigmaEC.evaluate.objective.ObjectiveFunction;
 import SigmaEC.represent.DoubleVectorIndividual;
 import SigmaEC.util.Misc;
+import SigmaEC.util.Parameters;
 import SigmaEC.util.math.Matrix;
 import java.util.Arrays;
 
@@ -18,19 +19,28 @@ import java.util.Arrays;
  * @author Eric 'Siggy' Scott
  * @author Jeff Bassett
  */
-public class AffineTransformedDoubleObjective extends ObjectiveFunction<DoubleVectorIndividual>
-{
+public class AffineTransformedDoubleObjective extends ObjectiveFunction<DoubleVectorIndividual> {
+    public final static String P_ANGLES = "angles";
+    public final static String P_SCALE = "scale";
+    public final static String P_OBJECTIVE = "objective";
+    
     private final double[][] transformationMatrix;
     private final ObjectiveFunction<DoubleVectorIndividual> objective;
     private final int numDimensions;
+    
+    public AffineTransformedDoubleObjective(final Parameters parameters, final String base) {
+        this(parameters.getDoubleArrayParameter(Parameters.push(base, P_ANGLES)),
+                parameters.getDoubleParameter(Parameters.push(base, P_SCALE)),
+                (ObjectiveFunction<DoubleVectorIndividual>) parameters.getInstanceFromParameter(Parameters.push(base, P_OBJECTIVE), ObjectiveFunction.class));
+        assert(repOK());
+    }
     
     /** Convenience constructor for creating affine transformation in R^2.
      * @param angles The angles by which the different axes will be rotated.
      * @param scale The value by which the function will be scaled.
      * @param objective The original objective function.
      */
-    public AffineTransformedDoubleObjective(final double[] angles, final double scale, final ObjectiveFunction<DoubleVectorIndividual> objective) throws IllegalArgumentException
-    {
+    public AffineTransformedDoubleObjective(final double[] angles, final double scale, final ObjectiveFunction<DoubleVectorIndividual> objective) throws IllegalArgumentException {
         this(getTransformationMatrix(angles, scale, objective.getNumDimensions()), objective);
     }
     
@@ -39,8 +49,7 @@ public class AffineTransformedDoubleObjective extends ObjectiveFunction<DoubleVe
      * augmented matrix.
      * @param objective The original objective function.
      */
-    public AffineTransformedDoubleObjective(final double[][] transformationMatrix, final ObjectiveFunction<DoubleVectorIndividual> objective) throws IllegalArgumentException
-    {
+    public AffineTransformedDoubleObjective(final double[][] transformationMatrix, final ObjectiveFunction<DoubleVectorIndividual> objective) throws IllegalArgumentException {
         if (objective == null)
             throw new IllegalArgumentException(this.getClass().getSimpleName() + ": objective is null.");
         if (transformationMatrix == null)
@@ -54,8 +63,7 @@ public class AffineTransformedDoubleObjective extends ObjectiveFunction<DoubleVe
     
     /** @return A matrix representation of an affine transformation
      * in R^2 with the specified rotations and scale . */
-    private static double[][] getTransformationMatrix(final double[] angles, final double scale, final int numDimensions) throws IllegalArgumentException
-    {
+    private static double[][] getTransformationMatrix(final double[] angles, final double scale, final int numDimensions) throws IllegalArgumentException {
         if (angles == null)
             throw new IllegalArgumentException(AffineTransformedDoubleObjective.class.getSimpleName() + ": angle array is null.");
         // N choose 2 = N (N-1) / 2
@@ -65,10 +73,8 @@ public class AffineTransformedDoubleObjective extends ObjectiveFunction<DoubleVe
         double[][] transformationMatrix = getScaleMatrix(scale, numDimensions);
 
         int angleIndex = 0;
-        for (int d1 = 0; d1 < numDimensions-1; d1++)
-        {
-            for (int d2 = d1 + 1; d2 < numDimensions; d2++)
-            {
+        for (int d1 = 0; d1 < numDimensions-1; d1++) {
+            for (int d2 = d1 + 1; d2 < numDimensions; d2++) {
                 double[][] rotMatrix = getAxisRotationMatrix(angles[angleIndex], d1, d2, numDimensions);
                 transformationMatrix = Matrix.multiply(transformationMatrix, rotMatrix);
                 angleIndex++;
@@ -80,8 +86,7 @@ public class AffineTransformedDoubleObjective extends ObjectiveFunction<DoubleVe
     
     /** @return A rotation matrix for a single axis.  The two dimensions
      * define the axis of rotation. */
-    private static double[][] getAxisRotationMatrix(final double angle, final int dimension1, final int dimension2, final int numDimensions) throws IllegalArgumentException
-    {
+    private static double[][] getAxisRotationMatrix(final double angle, final int dimension1, final int dimension2, final int numDimensions) throws IllegalArgumentException {
         if (dimension1 >= numDimensions || dimension2 >= numDimensions)
             throw new IllegalArgumentException(AffineTransformedDoubleObjective.class.getSimpleName() + ": bad dimension.");
         if (dimension1 == dimension2)
